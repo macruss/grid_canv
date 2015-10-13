a67e3b9c54f1e69f29ed328cc2b211b63f29aac6
Ruslan Makarenko <ruslan.makarenko@gmail.com>
2015-10-13 22:38:48 +0300

    added setScale method

--- src/main.ts
+++ src/main.ts
@@ -12,65 +12,75 @@ class Cell {
   private grid: Grid;
 
   constructor(x:number, y:number, grid: Grid) {
-    this.x = x;
-    this.y = y;
+    this.x     = x;
+    this.y     = y;
     this.color = 'black';
-    this.grid = grid;
+    this.grid  = grid;
   }
   public draw(): void {
-    var ctx = grid.ctx
+    var ctx  = grid.ctx
       , size = grid.cellSize
-      , x = toPixel(this.x, size)
-      , y = toPixel(this.y, size);
+      , x    = toPixel(this.x, size)
+      , y    = toPixel(this.y, size);
 
-    ctx.fillRect(x, y, size, size);
+    ctx.fillRect(x + 1, y + 1, size - 1, size - 1);
   };
 }
 
 
 interface GridConfig {
   canvas: HTMLCanvasElement,
-  cellSize?: number
+  scale?: number
 }
 class Grid {
-  private width: number;
+  private width:  number;
   private height: number;
-  private cells: Array<any>;
+  private cells:  Array<any>;
   private canvas: HTMLCanvasElement;
-  public ctx: any;
+  private scale:  number;
+  private beginX: number;
+  private beginY: number;
+  public ctx:     any;
   public cellSize: number;
 
   constructor(conf: GridConfig) {
-    this.canvas = conf.canvas;
-    this.ctx = conf.canvas.getContext('2d');
-    this.cellSize = conf.cellSize || 10;
-    this.width = toRelativeUnit(conf.canvas.width, this.cellSize);
-    this.height = toRelativeUnit(conf.canvas.height, this.cellSize);
-    this.cells = new Array(this.width * this.height);
+    this.canvas   = conf.canvas;
+    this.ctx      = conf.canvas.getContext('2d');
+    this.scale    = conf.scale;
+    this.cellSize = Math.floor(this.scale * 5);
+    this.width    = toRelativeUnit(conf.canvas.width, this.cellSize);
+    this.height   = toRelativeUnit(conf.canvas.height, this.cellSize);
+    this.beginX   = 0;
+    this.beginY   = 0;
+
+    this.cells    = new Array(this.width * this.height);
 
     this.draw();
+
     // events
-    this.canvas.onclick = (e) => this.handleClick(e)
+    this.canvas.onmouseup = (e) => this.handleClick(e)
+    this.canvas.onmousemove = (e) => this.handleMouseMove(e)
     this.canvas.onmousewheel = (e) => this.handleScroll(e)
   }
 
   private draw(): void {
-    var width = this.canvas.width 
+    var width  = this.canvas.width 
       , height = this.canvas.height
-      , size = this.cellSize;
+      , size   = this.cellSize
+      , x, y;
 
     this.ctx.beginPath();
 
-    for (var x = size + 0.5; x < width; x += size) {
+    for (x = this.beginX + size + 0.5; x < width; x += size) {
       this.ctx.moveTo(x, 0);
       this.ctx.lineTo(x, height);
     }
 
-    for (var y = size + 0.5; y < height; y += size) {
+    for (y = this.beginY + size + 0.5; y < height; y += size) {
       this.ctx.moveTo(0, y);
       this.ctx.lineTo(width, y);
     }
-    this.ctx.strokeStyle = 'gray';
+    this.ctx.strokeStyle = 'lightgray';
     this.ctx.stroke();
 
     this.cells.forEach((cell) => {
@@ -78,26 +88,25 @@ class Grid {
     });
   }
 
-  private setCell(x, y){
-    var cell = new Cell(x, y, this);
-    this.cells[x + this.width * y] = cell;
-    this.redraw();
+  private setCell(x, y) {
+    if (!this.cells[x + this.width * y]) {
+      this.cells[x + this.width * y] = new Cell(x, y, this);
+      this.redraw();
+    }
   }
 
   private getCell(x, y) {
     return this.cells[x + this.width * y];
   }
 
-  private handleClick(e) {
-    var x = toRelativeUnit(e.offsetX, this.cellSize)
-    var y = toRelativeUnit(e.offsetY, this.cellSize)
-    this.setCell(x, y);
+  public setScale(scale: number) {
+    this.scale = scale;
+    this.cellSize = Math.floor(this.scale * 5);
+    this.redraw();
   }
 
-  private handleScroll(e) {
-    this.cellSize += e.wheelDelta > 0 ? 5 : -5;
-    this.cellSize = Math.max(5, this.cellSize);
-    this.redraw();
+  public getScale() {
+    return this.scale;
   }
 
   private clearGrid() {
@@ -108,6 +117,28 @@ class Grid {
     this.clearGrid();
     this.draw();
   }
+
+  // event hendlers
+  private handleClick(e) {
+    var x = toRelativeUnit(e.offsetX, this.cellSize)
+      , y = toRelativeUnit(e.offsetY, this.cellSize)
+    this.setCell(x, y);
+  }
+
+  private handleMouseMove(e) {
+    if (e.which === 1) {
+      this.handleClick(e);
+    }
+  }
+
+  private handleScroll(e) {
+    var scale = this.scale;
+
+    scale += e.wheelDelta > 0 ? 0.25 : -0.25;
+    scale = Math.max(1, scale);
+    this.setScale(scale);
+  }
+
 }
 
 
@@ -115,7 +146,5 @@ var canvas = <HTMLCanvasElement>document.querySelector("#canvas");
 
 var grid = new Grid({
   canvas: canvas,
-  cellSize: 20,
+  scale: 3,
 });
-
-// grid.draw();
