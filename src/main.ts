4bcea1640efc3aa8ad677dc6bda7849ddcd54a87
Ruslan Makarenko <ruslan.makarenko@gmail.com>
2015-10-25 17:20:39 +0200

    added drag grid

--- src/main.ts
+++ src/main.ts
@@ -39,7 +39,7 @@ interface GridConfig {
   ratio: Array<number>
 }
 class Grid {
-  private wCells  : number;
+  private wCells : number;
   private hCells : number;
   private cells  : Array<any>;
   private canvas : HTMLCanvasElement;
@@ -47,6 +47,9 @@ class Grid {
   public ctx     : any;
   public cellSize: number;
   public mode    : string;
+  private zp     : { x: number, y: number };
+  private dragStart: any;
+
 
   constructor(conf: GridConfig) {
     this.canvas   = conf.canvas;
@@ -55,6 +58,7 @@ class Grid {
     this.cellSize = Math.floor(this.scale * 5);
     this.wCells   = conf.ratio[0];
     this.hCells   = conf.ratio[1];
+    this.zp       = {x: 0, y: 0};
     this.mode     = 'draw';
 
     this.cells    = new Array(this.wCells * this.hCells);
@@ -62,9 +66,10 @@ class Grid {
     this.draw();
 
     // events
-    this.canvas.onmouseup = (e) => this.handleClick(e)
-    this.canvas.onmousemove = (e) => this.handleMouseMove(e)
-    this.canvas.onmousewheel = (e) => this.handleScroll(e)
+    this.canvas.addEventListener('mouseup', (e) => this.handleMouseUp(e))
+    this.canvas.addEventListener('mousedown', (e) => this.handleMouseDown(e))
+    this.canvas.addEventListener('mousemove', (e) => this.handleMouseMove(e))
+    this.canvas.addEventListener('mousewheel', (e) => this.handleScroll(e))
   }
 
   static transformedPoint(x: number, y: number) {
@@ -79,12 +84,12 @@ class Grid {
 
     this.ctx.beginPath();
 
-    for (x = size + 0.5; x < width + size; x += size) {
+    for (x = 0.5; x < width + size; x += size) {
       this.ctx.moveTo(x, 0);
       this.ctx.lineTo(x, height);
     }
 
-    for (y = size + 0.5; y < height + size; y += size) {
+    for (y = 0.5; y < height + size; y += size) {
       this.ctx.moveTo(0, y);
       this.ctx.lineTo(width, y);
     }
@@ -103,18 +108,36 @@ class Grid {
     } else {
         this.cells[x + this.wCells * y].setColor(color)
     }
-  this.redraw();
-
+    this.redraw();
   }
 
   private getCell(x, y) {
     return this.cells[x + this.wCells * y];
   }
 
-  public setScale(scale: number) {
-    this.scale = scale;
+  public zoom(pt, zoomDirection: boolean) {
+    let delta = zoomDirection ? 0.25 : -0.25;
+
+    this.scale += delta
+    this.scale = Math.max(1, this.scale);
     this.cellSize = Math.floor(this.scale * 5);
     this.redraw();
+  }
+
+  public drag(e) {
+    if (this.dragStart) {
+      // console.log(e.offsetX - this.dragStart.x, e.offsetY - this.dragStart.y);
+      // this.zp.x += e.offsetX - this.dragStart.x;
+      // this.zp.y += e.offsetY - this.dragStart.y;
+      this.zp.x += e.movementX;
+      this.zp.y += e.movementY;
+      // console.log(this.zp);
+      this.clearGrid();
+      this.ctx.translate(e.movementX, e.movementY);
+      // console.log(e);
+      this.redraw()
+    }
+
   }
 
   public getScale() {
@@ -122,7 +145,7 @@ class Grid {
   }
 
   private clearGrid() {
-    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.ctx.clearRect(0 - this.zp.x, 0 - this.zp.y, this.canvas.width, this.canvas.height);
   }
 
   private redraw() {
@@ -131,35 +154,54 @@ class Grid {
   }
 
   private inGrid(x, y): boolean {
-    return x < this.wCells && y < this.hCells
+    return x >= 0 &&
+           x < this.wCells && 
+           y >= 0 && 
+           y < this.hCells
   }
 
   // event hendlers
-  private handleClick(e) {
+  private handleMouseUp(e) {
     var x, y;
 
-    x = toRelativeUnit(e.offsetX, this.cellSize);
-    y = toRelativeUnit(e.offsetY, this.cellSize);
+    if (this.mode === 'draw') {
+      x = toRelativeUnit(e.offsetX - this.zp.x, this.cellSize);
+      y = toRelativeUnit(e.offsetY - this.zp.y, this.cellSize);
 
-    if (this.inGrid(x, y)) {
-      this.setCell(x, y, colorpicker.value);
+      if (this.inGrid(x, y)) {
+        this.setCell(x, y, colorpicker.value);
+      }
+    } else {
+        this.dragStart = null;
     }
+
     return e.preventDefault() && false;
+  }
+
+  private handleMouseDown(e) {
+    if (this.mode === 'move') {
+      this.dragStart = { x: e.offsetX, y: e.offsetY };
+    }
   }
 
   private handleMouseMove(e) {
     if (e.which === 1) {
-      this.handleClick(e);
+      if (this.mode === 'draw') {
+        this.handleMouseUp(e);
+      } else {
+
+        this.drag(e);
+      }
+
     }
   }
 
   private handleScroll(e) {
-    var scale = this.scale;
-
-    scale += e.wheelDelta > 0 ? 0.25 : -0.25;
-    scale = Math.max(1, scale);
-
-    this.setScale(scale);
+    var pt = {
+        x: e.offsetX,
+        y: e.offsetY,
+      };
+    this.zoom(pt, e.wheelDelta > 0);
   }
 
 }
@@ -171,7 +213,7 @@ var $btns = <any>document.querySelectorAll(".actions button");
 
 var grid = new Grid({
   canvas: canvas,
-  ratio: [50, 50],
+  ratio: [70, 60],
   scale: 1.5,
 
 });
@@ -180,10 +222,30 @@ Array.prototype.forEach.call($btns, (el) => {
   el.addEventListener('click', handleClickActionsBtn) 
 });
 
+document.addEventListener('keyup', function(e) {
+  e.preventDefault();
+  if (e.altKey && e.ctrlKey) {
+      if (e.which === 68) { 
+        grid.mode = 'draw';
+        canvas.style.cursor = 'default';
+        $btns[0].classList.add('selected');
+        $btns[1].classList.remove('selected');
+      }
+      if (e.which === 77) {
+        grid.mode = 'move';
+        canvas.style.cursor = 'move';
+        $btns[1].classList.add('selected');
+        $btns[0].classList.remove('selected');
+      }
+  }
+})
+
 
 function handleClickActionsBtn (e) {
   grid.mode = e.currentTarget.id;
   if (!hasClass(e.currentTarget, 'selected')) {
+    grid.mode = e.currentTarget.id;
+    // console.log(grid.mode);
     toggleClass($btns, 'selected');
   }
 }
